d84f44f95c848a2e160dedfdabd18f945ecc7b88
Jesse Palmer <jesselpalmer@gmail.com>
2019-05-28 19:08:19 -0700

    test: add iscollidingtopleft test

---
+++ src/is-colliding-top/is-colliding-top-left/is-colliding-top-left.spec.ts
@@ -0,0 +1,22 @@
+import isCollidingTopLeft from '.'
+import { expect } from 'chai';
+
+describe('isCollidingTopLeft tests', () => {
+  it('should return true if object2 collides with the top left of object1', () => {
+    const object1 = {
+      x1: 5,
+      x2: 15,
+      y1: 5,
+      y2: 15
+    }
+
+    const object2 = {
+      x1: 0,
+      x2: 10,
+      y1: 0,
+      y2: 10
+    }
+
+    expect(isCollidingTopLeft(object1, object2)).to.be.true
+  })
+})
