b5e7baeaac5a35a540ee4adf6924145ee5981619
Jesse Palmer <jesselpalmer@gmail.com>
2020-11-23 00:07:32 -0800

    refactor: make methods private

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,6 @@
  * @fileoverview This is a library to detect collisions between two objects.
  * @author Jesse Palmer
  */
-
-import { isCollidingBottom, isCollidingBottomLeft, isCollidingBottomMiddle,
-  isCollidingBottomRight } from './is-colliding-top-bottom/is-colliding-bottom'
-import { isCollidingTop, isCollidingTopLeft, isCollidingTopMiddle, isCollidingTopRight } from './is-colliding-top-bottom/is-colliding-top'
-import isCollidingMiddleLeftRight from './is-colliding-middle-left-right'
-import isCollidingTopOrBottom from './is-colliding-top-bottom'
 import isColliding from './is-collision'
 
-export default {
-  isColliding,
-  isCollidingBottom,
-  isCollidingBottomLeft,
-  isCollidingBottomMiddle,
-  isCollidingBottomRight,
-  isCollidingMiddleLeftRight,
-  isCollidingTop,
-  isCollidingTopLeft,
-  isCollidingTopMiddle,
-  isCollidingTopOrBottom,
-  isCollidingTopRight,
-}
+export default isColliding
