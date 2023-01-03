ce8b4cc83957042b1875947aad57735254a64047
Jesse Palmer <jesselpalmer@gmail.com>
2023-01-02 17:47:42 -0800

    refactor: rename file

--- build/src/index.d.ts
+++ build/src/index.d.ts
@@ -2,5 +2,5 @@
  * @fileoverview This is a library to detect collisions between two objects.
  * @author Jesse Palmer
  */
-import isColliding from './is-collision';
+import isColliding from './is-colliiding';
 export default isColliding;
