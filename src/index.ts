7f4e49dba911212c99e436eff9b0e5b649782c8f
Jesse Palmer <jesselpalmer@gmail.com>
2023-01-02 17:48:26 -0800

    refactor: fix typo

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@
  * @fileoverview This is a library to detect collisions between two objects.
  * @author Jesse Palmer
  */
-import isColliding from './is-colliiding';
+import isColliding from './is-colliding';
 
 export default isColliding;
